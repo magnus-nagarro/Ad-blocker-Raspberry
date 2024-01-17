3b1a0f46fa51241b5d3d9238eba911bed5926429
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-01-17 13:31:58 +0100

    some fixes, backend running now

--- blocker/main.py
+++ blocker/main.py
@@ -1,5 +1,22 @@
 import requests
+from flask import Flask, jsonify, request, Response
+
+
+class Backend:
+    def __init__(self) -> None:
+        pass
+
+    def create_app(self):
+        app = Flask(__name__)
+
+        @app.route('/test', methods=['GET'])
+        def test():
+            return "Hello World"
+
+        return app
+
 
 if __name__ == "__main__":
-    while True:
-        continue
+    backend = Backend()
+    app = backend.create_app()
+    app.run(host='0.0.0.0', port=8080)
