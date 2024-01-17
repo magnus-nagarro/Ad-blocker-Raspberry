7eee34876936c2d872956bbebeaf8015a156b7ab
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-01-17 15:39:58 +0100

    added stuff to main func

--- blocker/main.py
+++ blocker/main.py
@@ -17,6 +17,11 @@ class Backend:
 
 
 if __name__ == "__main__":
-    backend = Backend()
-    app = backend.create_app()
-    app.run(host='0.0.0.0', port=8080)
+    try:
+        backend = Backend()
+        app = backend.create_app()
+        app.run(host='0.0.0.0', port=8080)
+    except Exception as e:
+        print(f"An error occured, error: {e}")
+    finally:
+        print("Exiting... Bye Bye :)")
