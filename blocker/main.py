a53406ca8259508f79553042f66e5a49ca15238a
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-01-22 15:09:32 +0100

    added endpoints for starting and stopping the blocker

--- blocker/main.py
+++ blocker/main.py
@@ -6,14 +6,17 @@ from MongoDB import mongodb
 class Backend(mongodb):
     def __init__(self) -> None:
         self.db = mongodb()
+        self.running = False
 
     def create_app(self):
         app = Flask(__name__)
 
+        # Test Endpoint, call with /test => [GET]
         @app.route("/test", methods=["GET"])
         def test():
             return "Hello World"
 
+        # Endpoint to add Links to the database, call with /add?link="" => [POST]
         @app.route("/add", methods=["POST"])
         def add_links():
             link = request.args.get('link')
@@ -21,6 +24,18 @@ class Backend(mongodb):
                 return jsonify("Please provide a link!")
             success = self.db.add_links(link)
             return jsonify(f"success: {success}")
+
+        # Endpoint to start the addblocker, call with /start => [POST]
+        @app.route("/start", methods=["POST"])
+        def start():
+            self.running = True
+            return jsonify(f"running: {self.running}")
+
+        # Endpoint to stop the addblocker, call with /stop => [POST]
+        @app.route("/stop", methods=["POST"])
+        def stop():
+            self.running = False
+            return jsonify(f"running: {self.running}")
 
         return app
 
