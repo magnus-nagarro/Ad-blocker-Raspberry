a6410c1e462345a546f674455e0d26d4e6272c61
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-02-08 19:53:52 +0100

    fix

--- blocker/Blocker.py
+++ blocker/Blocker.py
@@ -8,13 +8,16 @@ class blocker():
     def get_blocked_links(self):
         return self.db.return_links()
 
+    def packet_filter(self, packet):
+        if packet.haslayer(TCP):
+            return "google.com" in str(packet[TCP].payload)
+
     def blocker_loop(self):
         while True:
             running = self.db.should_blocker_run()
             if running:
-                s = socket.socket(
-                    socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-                incoming_packet = s.recvfrom(1024)
+                incoming_packet = sniff(
+                    filter="tcp", stop_filter=self.packet_filter)
                 blocked_list = self.get_blocked_links()
                 buff = {
                     "packet": incoming_packet
