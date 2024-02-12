20db6e90a7ba2e07d15572aa436850cab5044b5d
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-02-12 11:08:22 +0100

    Revert "fix"
    This reverts commit a6410c1e462345a546f674455e0d26d4e6272c61.

--- blocker/Blocker.py
+++ blocker/Blocker.py
@@ -8,16 +8,13 @@ class blocker():
     def get_blocked_links(self):
         return self.db.return_links()
 
-    def packet_filter(self, packet):
-        if packet.haslayer(TCP):
-            return "google.com" in str(packet[TCP].payload)
-
     def blocker_loop(self):
         while True:
             running = self.db.should_blocker_run()
             if running:
-                incoming_packet = sniff(
-                    filter="tcp", stop_filter=self.packet_filter)
+                s = socket.socket(
+                    socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+                incoming_packet = s.recvfrom(1024)
                 blocked_list = self.get_blocked_links()
                 buff = {
                     "packet": incoming_packet
--- blocker/__init__.py
+++ blocker/__init__.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 import threading
 import socket
 import time as sleep_time
-from scapy.all import sniff, TCP
 
 # own modules
 from MongoDB import mongodb
